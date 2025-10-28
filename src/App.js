2ffc32091002e84a4f989833f4b66119bfb3ac1d
Johnson Valenzuela <johnsondavisv4@hotmail.com>
2025-10-28 17:30:51 +0000

    feat: integrate Firebase for user management
    - Added Firebase SDK to package.json
    - Created firebaseConfig.js for Firebase initialization
    - Implemented Usuario component to manage user list
    - Added functionality to add new users and display existing users
    - Updated App.js to render Usuario component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./css/bootstrap.min.css";
 import "./css/bootstrap-icons.min.css";
 import { Fragment } from "react";
+import Usuario from "./components/Usuario";
 
 function App() {
   return (
-    <Fragment></Fragment>
+    <Usuario></Usuario>
   );
 }
 
